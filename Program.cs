7db3674f28e329c9ff2febe788725f65d17026d2
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-05 00:58:44 +0530

    UC4_SwitchCase

--- Program.cs
+++ Program.cs
@@ -4,11 +4,13 @@ namespace Coding_prac
 {
     class Program
     {
+        public const int is_part_time = 1;
+        public const int is_full_time = 2;
+
         static void Main(string[] args)
         {
             ///Constraints
-            int is_part_time = 1;
-            int is_full_time = 2;
+
             int emp_rate = 20;
 
             ///Variables
@@ -17,17 +19,18 @@ namespace Coding_prac
 
             Random random = new Random();
             int empcheck = random.Next(0, 2);
-            if (empcheck == is_part_time)
+
+            switch (empcheck)
             {
-                emphrs = 4;
-            }
-            else if (empcheck == is_full_time)
-            {
-                emphrs = 8;
-            }
-            else
-            {
-                emphrs = 0;
+                case is_part_time:
+                    emphrs = 4;
+                    break;
+                case is_full_time:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
             }
             empwage = emphrs * emp_rate;
             Console.WriteLine("Empployee wage" + empwage);
