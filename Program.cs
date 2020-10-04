fd6f525e924d03b567851e33701ccfc18a32bee5
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-05 02:55:21 +0530

    UC11_Interface

--- Program.cs
+++ Program.cs
@@ -1,47 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Coding_prac
 {
-    class Program
+    interface ICompany
+    {
+        public abstract int CalWage(String compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon);
+    }
+    public class Company : ICompany
     {
         ///Constraints
         public const int is_part_time = 1;
         public const int is_full_time = 2;
-        //public const int emp_rate_per_hr = 20;
-        //public const int no_work_days = 2;
-        //public const int max_hrs_in_mon = 100;
 
-        static void Main(string[] args)
+        public string compName { get; set; }
+        public int emp_rate_perHr { get; set; }
+        public int no_work_days { get; set; }
+        public int max_hrs_in_mon { get; set; }
+        public int cal_wage { get; set; }
+        //string compName, int emp_rate_perHr, int no_work_days, int max_hrs_in_mon, int cal_wage
+        public string CompanyDet()
         {
-            Console.WriteLine("Employee Wage Prob");
-            List<String> Emp = new List<String>();
-            Console.WriteLine("Enter no of companies: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
-            {
-                List<String> b = new List<String>();
-                Console.WriteLine("Comp Name");
-                String compName1 = Console.ReadLine();
-                b.Add(compName1);
-                Console.WriteLine("Emp Rate");
-                String emp_rate_per_hr = Console.ReadLine();
-                b.Add(emp_rate_per_hr);
-                Console.WriteLine("No. of working hrs");
-                String no_of_work_days = Console.ReadLine();
-                b.Add(no_of_work_days);
-                Console.WriteLine("Max work hrs in a month");
-                String max_hrs_in_mon = Console.ReadLine();
-                b.Add(max_hrs_in_mon);
-                int cw1 = CalEmpWage(compName1, Convert.ToInt32(emp_rate_per_hr), Convert.ToInt32(no_of_work_days), Convert.ToInt32(max_hrs_in_mon));
-                Console.WriteLine("Total wage for " + compName1 + " is " + cw1);
-                b.Add(cw1.ToString());
-                Emp.AddRange(b);
-            }
-            Emp.ForEach(Console.WriteLine);
+            return string.Format("{0} is an author of {1}. Price: ${2}", compName, emp_rate_perHr, no_work_days, max_hrs_in_mon, cal_wage);
         }
-
-        public static int CalEmpWage(String compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon)
+        public int CalWage(String compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon)
         {
             ///Variables
             int emphrs = 0;
@@ -72,6 +55,52 @@ namespace Coding_prac
             Console.WriteLine("Employee wage for company " + compName + " is " + totalempwage);
             return totalempwage;
         }
+    }
+    class Program
+    {
+
+        //public const int emp_rate_per_hr = 20;
+        //public const int no_work_days = 2;
+        //public const int max_hrs_in_mon = 100;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Wage Prob");
+            List<Company> Emp = new List<Company>();
+            Console.WriteLine("Enter no of companies: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                List<String> b = new List<String>();
+                Console.WriteLine("Comp Name");
+                String compName1 = Console.ReadLine();
+                //b.Add(compName1);
+                Console.WriteLine("Emp Rate");
+                String emp_rate_per_hr = Console.ReadLine();
+                //b.Add(emp_rate_per_hr);
+                Console.WriteLine("No. of working hrs");
+                String no_of_work_days = Console.ReadLine();
+                //b.Add(no_of_work_days);
+                Console.WriteLine("Max work hrs in a month");
+                String max_hrs_in_mon = Console.ReadLine();
+                //b.Add(max_hrs_in_mon);
+                Company c = new Company();
+                int cw1 = c.CalWage(compName1, Convert.ToInt32(emp_rate_per_hr), Convert.ToInt32(no_of_work_days), Convert.ToInt32(max_hrs_in_mon));
+                Console.WriteLine("Total wage for " + compName1 + " is " + cw1);
+                //b.Add(cw1.ToString());
+                Emp.Add(new Company { compName = compName1, emp_rate_perHr = Convert.ToInt32(emp_rate_per_hr), no_work_days = Convert.ToInt32(no_of_work_days), max_hrs_in_mon = Convert.ToInt32(max_hrs_in_mon), cal_wage = cw1 });
+                //Emp.Add(new company);
+                //Emp.AddRange(b);
+            }
+            foreach (Company i in Emp)
+            {
+                Console.WriteLine($"Company Details: {i.compName}:{i.emp_rate_perHr}:{i.no_work_days}:{i.max_hrs_in_mon}:{i.cal_wage}");
+            }
+        }
+
+
+
+
 
 
     }
