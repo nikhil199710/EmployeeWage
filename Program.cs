fd4a42302f74d1115845e6d5c29e2c4d1ebc1392
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-10-06 07:59:06 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,46 @@
-﻿using System;
+using System;
 
 namespace Coding_prac
 {
     class Program
     {
+        ///Constraints
         public const int is_part_time = 1;
         public const int is_full_time = 2;
+        public const int emp_rate_per_hr = 20;
+        public const int no_work_days = 2;
+        public const int max_hrs_in_mon = 10;
 
         static void Main(string[] args)
         {
-            ///Constraints
-
-            int emp_rate = 20;
 
             ///Variables
             int emphrs = 0;
-            int empwage = 0;
-
-            Random random = new Random();
-            int empcheck = random.Next(0, 2);
-
-            switch (empcheck)
+            int totalemphrs = 0;
+            int totalworkdays = 0;
+            while (totalemphrs <= max_hrs_in_mon && totalworkdays < no_work_days)
             {
-                case is_part_time:
-                    emphrs = 4;
-                    break;
-                case is_full_time:
-                    emphrs = 8;
-                    break;
-                default:
-                    emphrs = 0;
-                    break;
+                totalworkdays++;
+                Random random = new Random();
+                int empcheck = random.Next(0, 2);
+
+                switch (empcheck)
+                {
+                    case is_part_time:
+                        emphrs = 4;
+                        break;
+                    case is_full_time:
+                        emphrs = 8;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+                totalemphrs += emphrs;
+                Console.WriteLine("Days:" + totalworkdays + "Emp Hrs:" + emphrs);
             }
-            empwage = emphrs * emp_rate;
-            Console.WriteLine("Empployee wage" + empwage);
+            int totalempwage = totalemphrs * emp_rate_per_hr;
+            Console.WriteLine("Empployee wage" + totalempwage);
 
         }
     }
