ae80af8ecfb463ab34ad78eb31c5d57b75394d5f
nikhil199710 <71897566+nikhil199710@users.noreply.github.com>
2020-11-28 14:57:42 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,64 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
 
-namespace Coding_prac
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmployeeWage.cs" company="Bridgelabz">
+//   Copyright © 2018 Company// </copyright>
+// <creator Name="Nikhil Kumar Yadav"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DailyWageProblem
 {
-    class Program
+    class EmployeeWageComputation
     {
-        ///Constraints
-        public const int is_part_time = 1;
-        public const int is_full_time = 2;
-        public const int emp_rate_per_hr = 20;
-        public const int no_work_days = 2;
-        public const int max_hrs_in_mon = 100;
+        const int IS_FULL_TIME = 1;
+        const int ISS_PART_TIME = 2;
+        private string companyName;
+        private int maxWorkingHours;
+        private int maxNumOfWorkingDays;
+        private int ratePerHour;
+        private int totalWage = 0;
 
-        static void Main(string[] args)
+        public EmployeeWageComputation(string companyName, int maxWorkingHours, int maxNumOfWorkingDays, int ratePerHour)
         {
-            Console.WriteLine("Employee Wage Prob");
-            Console.WriteLine("Comp Name");
-            String compName1 = Console.ReadLine();
-            int cw1 = CalEmpWage(compName1, 28, 8, 9);
-            Console.WriteLine("Total wage for " + compName1 + " is " + cw1);
-            Console.WriteLine("Comp Name");
-            String compName2 = Console.ReadLine();
-            int cw2 = CalEmpWage(compName2, 40, 5, 6);
-            Console.WriteLine("Total wage for " + compName2 + " is " + cw2);
+            this.companyName = companyName;
+            this.maxWorkingHours = maxWorkingHours;
+            this.maxNumOfWorkingDays = maxNumOfWorkingDays;
+            this.ratePerHour = ratePerHour;
 
         }
 
-        public static int CalEmpWage(String compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon)
+        public void calculateEmpWage()
         {
-            ///Variables
-            int emphrs = 0;
-            int totalemphrs = 0;
-            int totalworkdays = 0;
-            while (totalemphrs <= max_hrs_in_mon && totalworkdays < no_work_days)
-            {
-                totalworkdays++;
-                Random random = new Random();
-                int empcheck = random.Next(0, 3);
+            int empHrs;
 
+            int totalEmpHours = 0;
+            int workingDays = 0;
+            Random rand = new Random();
+            while (totalEmpHours < maxWorkingHours && workingDays < maxNumOfWorkingDays)
+
+            {
+
+                int empcheck = rand.Next(0, 3);
                 switch (empcheck)
                 {
-                    case is_part_time:
-                        emphrs = 4;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
                         break;
-                    case is_full_time:
-                        emphrs = 8;
+                    case ISS_PART_TIME:
+                        empHrs = 4;
                         break;
                     default:
-                        emphrs = 0;
+                        empHrs = 0;
                         break;
                 }
-                totalemphrs += emphrs;
-                Console.WriteLine("Days:" + totalworkdays + "Emp Hrs:" + emphrs);
+                totalEmpHours = totalEmpHours + empHrs;
+                Console.WriteLine("the total working days is " + workingDays + "and working hours is " + totalEmpHours);
+
+                workingDays++;
             }
-            int totalempwage = totalemphrs * emp_rate_per_hr;
-            Console.WriteLine("Employee wage for company " + compName + " is " + totalempwage);
-            return totalempwage;
+
+            totalWage = totalEmpHours * ratePerHour;
+            Console.WriteLine("The total wage of worker for" + companyName + "is:" + totalWage);
+
+
         }
-
-
+         public string information()
+        {
+            return "Total wage for" + this.companyName + "is:" + this.totalWage;
+        }
     }
 
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+                EmployeeWageComputation TATA = new EmployeeWageComputation("Dmart", 60, 50, 20);
+                EmployeeWageComputation Amazon = new EmployeeWageComputation("Reliance", 40, 70, 50);
+                Dmart.calculateEmpWage();
+                Reliance.calculateEmpWage();
+                Console.WriteLine(Dmart.information());
+                Console.WriteLine(Reliance.information());
+            }
+        }
 }
